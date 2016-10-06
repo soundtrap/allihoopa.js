87fb0e79f7d0e297a49b52533b18bf0d0827dc71
mhallin <mhallin@fastmail.com>
2016-10-06 17:16:13 +0200

    Show auth dialog before dropping

--- src/drop.tsx
+++ src/drop.tsx
@@ -5,9 +5,19 @@ import {Coordinator} from './drop/components/Coordinator';
 import {Overlay} from './drop/components/Overlay';
 import {DropPiece} from './drop/PieceData';
 
+import {authenticate} from './auth';
+
 export type DropCallback = (successful: boolean) => void;
 
 export function drop(props: DropPiece, callback: DropCallback) {
+    authenticate(success => {
+        if (success) {
+            renderDrop(props, callback);
+        }
+    });
+}
+
+function renderDrop(piece: DropPiece, callback: DropCallback) {
     const container = document.createElement('div');
     document.body.appendChild(container);
 
@@ -18,7 +28,7 @@ export function drop(props: DropPiece, callback: DropCallback) {
     ReactDOM.render(
         <Overlay>
             <Coordinator
-                input={props}
+                input={piece}
                 onClose={onClose}
             />
         </Overlay>,
