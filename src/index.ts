bbcbf6d1122d0ca55ddf3d671bd726f4e74b550b
Kenneth Van der werf <kennethvanderwerf@Kenneths-MacBook-Pro.local>
2016-10-19 16:48:06 +0200

    Open Raw API calls for external use

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export { setup } from './config';
 export { authenticate } from './auth';
 export { drop } from './drop';
+export {
+  uploadResource as upload,
+  dropPiece as dropUpload
+} from './drop/DropAPI';
 export { DropPiece } from './drop/PieceData';
